4cb9fcbb33562a307daac069e0de1926cb4b49dc
juan20carlos <juancarlos14089@gmail.com>
2021-11-12 23:32:08 +0000

    principal

---
+++ print.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+int _printf(const char * const format, ...)
+{
+	va_list ap;
+	int i, j, l = 0;
+	int contador = 0;
+
+	va_start(ap, format);
+	if (format == NULL)
+	{
+		exit(98);
+	}
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; p[j].name; j++)
+			{
+				if(format[i + 1] == *p[j].name)
+				{
+					l += p[j].f(ap);
+					break;
+				}
+			}
+			if(!p[j].name && format[i + 1] != '\0')
+			{
+				_putchar(format[i]);
+				i--;
+				l++;
+			}
+			if (!p[j].name && format[i + 1] == '\0')
+			{
+				return (-1);
+			}
+			i++;
+		}
+		else if (format [i] != '%')
+		{
+			_putchar(format[i]);
+			contador++;
+		}
+	}
+	va_end (ap);
+	return (contador + l);
+}
