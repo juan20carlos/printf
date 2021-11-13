e423b54883e4029915a0beb016a76786cc793fd7
juan20carlos <juancarlos14089@gmail.com>
2021-11-13 00:29:21 +0000

    funcion 1

--- caracteres_1.c
+++ caracteres_1.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * _len: la longitud de una cadena
+ * @s: comprueba la cadena
+ * Return: always 0
  */
 int _len(char *s)
 {
 	int leng = 0;
+
 	while (s[leng] != '\0')
 		leng++;
 	return (leng);
 }
 /**
- *
- *
- *
- *
- *
+ * print_chars - imprimir caracteres
+ * @str: cadena pasada
+ * Return: len str
  */
 int print_chars(char *str)
 {
 	int i = 0;
-	if(str)
+
+	if (str)
 	{
 		while (*str)
 		{
@@ -31,16 +30,14 @@ int print_chars(char *str)
 			i++;
 		}
 	}
-	return(i);
+	return (i);
 }
 /**
- *
- *
- *
- *
- *
+ * rev_string - Función que invierte una cadena
+ * @s: cadena
+ * Return: nada
  */
-void rev_string(char*s)
+void rev_string(char *s)
 {
 	int i, j, k;
 	char c[1024];
@@ -61,16 +58,15 @@ void rev_string(char*s)
 	}
 }
 /**
- *
- *
- *
- *
- *
+ *  print_number_flag - Imprime un número primo
+ *  @n: marque n
+ * @flag: indicador de bandera
+ * Return: 1 si el entero de entrada es un número primo
  */
 void print_number_flag(int n, int flag)
 {
 	unsigned int n1;
-	
+
 	if (n < 0 && flag == 0)
 	{
 		_putchar('-');
@@ -88,25 +84,25 @@ void print_number_flag(int n, int flag)
 	{
 		print_number_flag(n1 / 10, flag);
 	}
-	_putchar((n1%10) + '0');
+	_putchar((n1 % 10) + '0');
 }
 /**
- *
- *
- *
- *
- *
+ * count_digit_base_flag - Dígitos de recuento de funciones
+ * @i: Marque i
+ * @base: comprobar base
+ *  @flag: indicador de bandera (ej. "% d" -> 0, "% u" -> 1)
+ * Return: i
  */
 int count_digit_base_flag(int i, int base, int flag)
 {
 	unsigned int j = 0;
 	unsigned int k;
-	
-	if(i == 0)
+
+	if (i == 0)
 	{
 		return (1);
 	}
-	if(i < 0 && flag == 0)
+	if (i < 0 && flag == 0)
 	{
 		k = i * -1;
 		j++;
