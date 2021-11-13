5dbdfdf7db5ca543b4d638b75e563ec43ade2d7f
juan20carlos <juancarlos14089@gmail.com>
2021-11-13 00:26:21 +0000

    principal

--- print.c
+++ print.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _printf - imprime en formato printf
+ * @format: puntero
+ * Return: retorna el valor
  */
-
 int _printf(const char * const format, ...)
 {
 	va_list ap;
@@ -13,22 +12,18 @@ int _printf(const char * const format, ...)
 
 	va_start(ap, format);
 	if (format == NULL)
-	{
 		exit(98);
-	}
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			for (j = 0; p[j].name; j++)
 			{
-				if(format[i + 1] == *p[j].name)
-				{
+				if (format[i + 1] == *p[j].name)
 					l += p[j].f(ap);
 					break;
-				}
 			}
-			if(!p[j].name && format[i + 1] != '\0')
+			if (!p[j].name && format[i + 1] != '\0')
 			{
 				_putchar(format[i]);
 				i--;
@@ -40,12 +35,12 @@ int _printf(const char * const format, ...)
 			}
 			i++;
 		}
-		else if (format [i] != '%')
+		else if (format[i] != '%')
 		{
 			_putchar(format[i]);
 			contador++;
 		}
 	}
-	va_end (ap);
+	va_end(ap);
 	return (contador + l);
 }
