8dd13a82d7d072fb63f26f2309d937b7e8777679
juan20carlos <juancarlos14089@gmail.com>
2021-11-13 00:28:26 +0000

    funcion

--- caracteres.c
+++ caracteres.c
@@ -1,40 +1,35 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * print_char - imprime el caracter
+ * @c: argumento
+ *Return: 1
  */
 int print_char(va_list c)
 {
-	_putchar(va_arg(c,int));
-	return(1);
+	_putchar(va_arg(c, int));
+	return (1);
 }
-
 /**
- *
- *
- *
- *
+ * print_str - imprime caracter
+ * @s: argumento
+ * Return: i
  */
 int print_str(va_list s)
 {
 	char *str = va_arg(s, char *);
 	int i = 0;
-	 
+
 	if (str == NULL)
 	{
-		str = "(null)";
+		str = "(nill)";
 	}
 	i = print_chars(str);
-	return(i);
+	return (i);
 }
 /**
- *
- *
- *
- *
- *
+ * print_porc - funcion imprime el %
+ * @p: variable
+ * Return: 1
  */
 int print_porc(va_list __attribute__((unused)) p)
 {
